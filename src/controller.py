d0271d6ce60fe4429e9466b6421af9b7f136bac3
jjh9397 <jasonhuang2023@u.northwestern.edu>
2022-07-27 20:20:52 -0500

    wip move input processing
    Co-authored-by: liudev777 <liudev777@users.noreply.github.com>

--- src/controller.py
+++ src/controller.py
@@ -1,3 +1,5 @@
+#from curses.ascii import isdigit
+from regex import E
 from requests import post
 from settings import Position
 from model import Model
@@ -11,38 +13,74 @@ class Controller():
         pass
     
     def processInput(self, notation: str):
-        validNotation = 'abcdefgh'
-        n = notation #ex: Pe4
-        targetNotation = n[-2:] #ex: e4
-        if targetNotation[0] not in validNotation:
-            raise IndexError("invalid row")
-        if int(targetNotation[1]) < 1 or int(targetNotation[1]) > 8:
-            raise IndexError("invalid column")
-        targetPostition = Position(ord(targetNotation[0]) - 97, int(targetNotation[1]) - 1)
-        l = len(n)
-        if l == 2:
-            pieces = self.m.piece_dict["P"]
-        if l >= 3:
-            pieces = self.m.piece_dict[n[0]] #ex: 'N'
-            if l == 4:
-                file = n[1]
-                if file.isdigit():
-                    col = file
-                    print ('col', targetPostition)
-                else:
-                    row = 97 - ord(file)
-                    print ('row', targetPostition)
-        targetPiece = []
-        print("pieces", pieces)
-        for piece in pieces:
-            if targetPostition in piece.moves:
-                print("------contains move!!!------")
-                targetPiece.append(piece)
+        validColumns = 'abcdefgh'
+        capture = False
+        n = notation #ex: Ne4
+        origin = None
+        rank = None
+        file = None
+        count = 0
 
-        if len(targetPiece) > 2 and l < 4:
-            raise (f"invalid notation, there are multiple {n[0]} pieces that can move to {n[-2:]}!")
-        else:
-            print(targetPiece)
+        destinationNotation = n[-2:] #ex: e2
+        targetPiece = [] #potential pieces that can move that the destination
+
+        if destinationNotation[0] not in validColumns:
+            raise IndexError("invalid destination column")
+        if int(destinationNotation[1]) < 1 or int(destinationNotation[1]) > 8:
+            raise IndexError("invalid destination row")
+        destination = Position(ord(destinationNotation[0]) - 97, int(destinationNotation[1]) - 1) 
+        l = len(n) # length of the notation
+
+        if l == 2: #e4
+            pieces = self.m.piece_dict["P"]
+            
+            for piece in pieces: 
+                    if destination in piece.moves:
+                        count += 1
+                        if count == 2:
+                            raise ("ambiguous notation")
+                        origin = piece.position
+        
+        if n[0] in validColumns: #exd5
+            for piece in pieces:
+                    if piece.position.y == rank or piece.position.x == file:
+                        origin = piece.position
+
+        if l >= 3: #Nf3, Rdf8, R1a3, Qh4e1, Bxc6, Rdxf8, R1xa3, Qh4xe1
+            pieces = self.m.piece_dict[n[0]] #ex: 'K'
+            if n[-3] == "x": #checks if user wants to take, speeds up taking process. If there is no valid piece to take, send error. (Note user doesn't need 'x' to take)
+                l -= 1
+                capture = True
+            if l == 3:
+                for piece in pieces: 
+                    if destination in piece.moves:
+                        count += 1
+                        if count == 2:
+                            raise ("ambiguous notation")
+                        origin = piece.position
+            if l == 4:
+                if n[1].isnumeric():
+                    col = n[1]
+                else:
+                    row = 97 - ord(n[1])
+                for piece in pieces:
+                        if piece.position.y == rank or piece.position.x == file:
+                            origin = piece.position
+            if not origin or origin.x == None or origin.y == None:
+                raise ("specified piece doesn't exist")
+            if l == 5: #ex: Qc3d4
+                origin = Position(ord(n[1]) - 97, int(n[2]) - 1)
+                if not m.board[origin.x][origin.y]:
+                    raise ("specified piece doesn't exist")
+                # add code
+        #print("pieces", pieces)
+        print(f"origin: {origin}")
+        print(f"destination: {destination}")
+
+        #if len(targetPiece) > 2 and l < 4: #checks if only one piece has the destination, otherwise throw error.
+        #    raise (f"ambiguous notation, there are multiple {n[0]} pieces that can move to {n[-2:]}!")
+        #else:
+        #    print(targetPiece)
 
         # print('process input pieces', pieces)
         # print(targetPostition)
@@ -52,10 +90,11 @@ m = Model()
 m.calculateAll()
 print(m)
 c = Controller(m)
-# c.processInput("N7e3")
-# c.processInput("Pec7")
-# c.processInput("e3")
-# c.processInput("e4")
+c.processInput("e4")
+c.processInput("e5")
 c.processInput("Nf3")
-c.processInput("f")
+c.processInput("Nc6")
+c.processInput("Bb5")
+c.processInput("a6")
+c.processInput("Bxc6")
 
