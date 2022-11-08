3767564aa5d45c83916bb4e0709b9fe64a688991
unknown <liudev777@gmail.com>
2022-11-07 20:33:55 -0600

    init view

---
+++ src/view.py
@@ -0,0 +1,35 @@
+from model import Model
+
+m = Model()
+board = m.board
+board[0][3] = None
+board[0][4] = None
+board[0][5] = None
+
+print(board)
+
+def toFEN(board):
+    fen = []
+    none_count = 0
+    for row in board:
+        fen_row = []
+        for piece in row:
+            if piece:
+                if none_count:
+                    print(none_count)
+                    fen_row.append((none_count))
+                    none_count = 0
+                if piece.color == 'w':
+                    notation = piece.type.upper()
+                elif piece.color == 'b':
+                    notation = piece.type.lower()
+                fen_row.append(notation)
+            else:
+                none_count += 1
+        if none_count:
+            fen_row.append(none_count)
+            none_count = 0
+        fen.append(fen_row)
+    print(fen)
+
+toFEN(board)
