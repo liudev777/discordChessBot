d7dc164672a6e798c7f12167212005cd3031c62a
unknown <liudev777@gmail.com>
2022-11-17 21:08:57 -0600

    moving pieces with notation

--- src/tests.py
+++ src/tests.py
@@ -4,22 +4,22 @@ from settings import Position
 from view import *
 
 
-m = Model() 
+t = Model() 
 # print(board)
 
-m.movePiece((Position(1,0)), Position(1,2))
-m.movePiece((Position(1,1)), Position(3,3))
-m.movePiece((Position(7,0)), Position(1,5))
-m.movePiece((Position(0,7)), Position(3,2))
-m.movePiece((Position(3,1)), Position(5,5))
-m.movePiece((Position(4,0)), Position(3,2))
-m.movePiece((Position(4,1)), Position(7,4))
-m.movePiece((Position(5,1)), Position(5,2))
-m.movePiece((Position(7,7)), Position(3,3))
+t.movePiece((Position(1,0)), Position(1,2))
+t.movePiece((Position(1,1)), Position(3,3))
+t.movePiece((Position(7,0)), Position(1,5))
+t.movePiece((Position(0,7)), Position(3,2))
+t.movePiece((Position(3,1)), Position(5,5))
+t.movePiece((Position(4,0)), Position(3,2))
+t.movePiece((Position(4,1)), Position(7,4))
+t.movePiece((Position(5,1)), Position(5,2))
+t.movePiece((Position(7,7)), Position(3,3))
 
-board = m.board
-m.printBoard()
-toFEN(m.board)
+board = t.board
+t.printBoard()
+toFEN(t.board)
 
 # pp(board)
 
